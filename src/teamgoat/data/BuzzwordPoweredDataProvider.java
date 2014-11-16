432ba0b6450cc70f07d8ea675671343bed973108
chris rehfeld <rehfeldchris@gmail.com>
2014-11-15 16:06:23 -0800

    added apache commons lib since we will likely use an sql interface for data access

--- src/teamgoat/data/BuzzwordPoweredDataProvider.java
+++ src/teamgoat/data/BuzzwordPoweredDataProvider.java
@@ -10,7 +10,6 @@ import teamgoat.entity.User;
 import teamgoat.entity.UserLocationSnapshot;
 
 public class BuzzwordPoweredDataProvider implements UserLocationDataProvider {
-	private Statement userlocatiponStmt;
 
 	public List<UserLocationSnapshot> getUsersWithinRange(TemporalLocation temporalLocation, double maxRangeInMeters) throws DataAccessException {
 		return null;
